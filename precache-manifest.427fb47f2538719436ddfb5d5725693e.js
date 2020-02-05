5f31cc686361fa8c03d6ea29be402d38ca6071fc
SheiUn <me.sheiun@gmail.com>
2020-02-05 13:58:47 +0800

    Updates

---
+++ precache-manifest.427fb47f2538719436ddfb5d5725693e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67e35f8e1a5c4afd053f",
+    "url": "/css/app.1f2146ff.css"
+  },
+  {
+    "revision": "3fbb4be3657afa5ba42d",
+    "url": "/css/chunk-vendors.a4393e1d.css"
+  },
+  {
+    "revision": "e245273bd69e3e102382c5b37f6164a6",
+    "url": "/img/logo.e245273b.png"
+  },
+  {
+    "revision": "c4cfb3ac53a6aabe58e616d530438143",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67e35f8e1a5c4afd053f",
+    "url": "/js/app.760497f6.js"
+  },
+  {
+    "revision": "3fbb4be3657afa5ba42d",
+    "url": "/js/chunk-vendors.c41a347e.js"
+  },
+  {
+    "revision": "2a88ad9db5a6ce427e13dbb2059bf151",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
