93ca76c1b81aed6fa3017859b01b04a4d360c8f0
SheiUn <me.sheiun@gmail.com>
2020-02-05 23:15:53 +0800

    chore: add link to text of source  code

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,7 +5,8 @@
         <strong>地下城與美食</strong> 由
         <a href="https://www.plurk.com/DungeonsDelicacy">DDY地下城與美食協會</a>
         舉辦｜<strong>排行榜</strong> 由
-        <a href="https://github.com/sheiun">SheiUn</a> 開發 原始碼採用
+        <a href="https://github.com/sheiun">SheiUn</a> 開發
+        <a href="https://github.com/sheiun/DungeonsDelicacy">原始碼</a> 採用
         <a href="http://opensource.org/licenses/mit-license.php">MIT</a> 授權｜
         <strong>Logo</strong> 遵照
         <a href="https://www.plurk.com/p/no6yis">此噗</a> 的使用範圍
