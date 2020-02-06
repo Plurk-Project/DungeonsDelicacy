17413b1c028c59a5df22082cb02ccb3b87edba08
SheiUn <me.sheiun@gmail.com>
2020-02-07 01:19:29 +0800

    feat: add broken chars

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -7,10 +7,37 @@
       <b-navbar-item>
         <h1><strong>地下城與美食 非官方 排行榜</strong></h1>
       </b-navbar-item>
+      <b-navbar-item>
+        <b-button
+          @click="showBrokenChars()"
+          type="is-primary"
+          size="is-small"
+          icon-left="account-remove"
+        >
+        </b-button>
+      </b-navbar-item>
     </template>
   </b-navbar>
 </template>
 
+<script>
+export default {
+  methods: {
+    showBrokenChars() {
+      const chars = this.$store.getters.getBrokenChars;
+      this.$buefy.dialog.alert({
+        title: '角卡格式不符或沒有權限名單',
+        message: `<div class="list">${chars
+          .map(this.itemWrapper)
+          .join('')}</div>`,
+      });
+    },
+    itemWrapper(item) {
+      return `<a class="list-item">${item}</a>`;
+    },
+  },
+};
+</script>
 <style lang="scss">
 .navbar .navbar-brand {
   text-align: center;
