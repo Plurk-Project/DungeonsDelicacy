a50d159c04372e0d7fee0538d8c1ce650527d42f
SheiUn <me.sheiun@gmail.com>
2020-02-06 19:51:48 +0800

    feat: add snackbar and notification

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,8 @@
 import Navbar from './components/Navbar.vue';
 import Rank from './components/Rank.vue';
 import Footer from './components/Footer.vue';
-import { ToastProgrammatic as Toast } from 'buefy';
+import { SnackbarProgrammatic as Snackbar } from 'buefy';
+import { NotificationProgrammatic as Notification } from 'buefy';
 
 export default {
   name: 'app',
@@ -31,12 +32,19 @@ export default {
           this.$store.commit('setChars', { data: res.data });
         }
         loading.close();
-        Toast.open({
-          duration: 10000,
-          message:
-            '如果你找不到你的角色名稱<br />那可能是你的 角卡格式 <em>不正確</em> 或沒有開啟權限<br />請按照官方規格角卡並請勿更動基本資料頁面的儲存格',
-          position: 'is-bottom',
+
+        Snackbar.open({
+          duration: 3000,
+          message: '每 8 小時自動更新一次',
+          position: 'is-top',
           type: 'is-info',
+        });
+
+        Notification.open({
+          indefinite: true,
+          type: 'is-info',
+          message:
+            '如果你找不到你的 <em>角色名稱</em><br />那可能是你的 角卡格式 <em>不正確</em> 或沒有開啟權限<br />請按照官方規格角卡並請勿更動基本資料頁面的儲存格',
         });
       });
   },
