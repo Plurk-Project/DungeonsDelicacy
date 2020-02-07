3e807b78d261a0a2e046c8000d14c9203acdefb9
SheiUn <me.sheiun@gmail.com>
2020-02-07 19:41:28 +0800

    refactor: #2

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -9,30 +9,38 @@
 </template>
 
 <script>
+const tabs = [
+  '經驗',
+  '身高',
+  '體重',
+  '體力',
+  '金錢',
+  '命中',
+  '傷害',
+  '防禦',
+  '迴避',
+  '法傷',
+  '治療',
+  '料理',
+  '採集',
+  '體能',
+  '偵查',
+  '鑑定',
+  '鍛造',
+  '開鎖',
+  '扒竊',
+];
+
+const getField = (item) => ({
+  field: item,
+  label: item,
+  visible: false,
+});
+
 export default {
   props: ['tab'],
   data: () => ({
-    tabs: [
-      '經驗',
-      '身高',
-      '體重',
-      '體力',
-      '金錢',
-      '命中',
-      '傷害',
-      '防禦',
-      '迴避',
-      '法傷',
-      '治療',
-      '料理',
-      '採集',
-      '體能',
-      '偵查',
-      '鑑定',
-      '鍛造',
-      '開鎖',
-      '扒竊',
-    ],
+    tabs,
     data: [],
     columns: [
       {
@@ -45,101 +53,7 @@ export default {
         field: '名稱',
         label: '角色名稱',
       },
-      {
-        field: '經驗',
-        label: '經驗',
-        visible: false,
-      },
-      {
-        field: '身高',
-        label: '身高',
-        visible: false,
-      },
-      {
-        field: '體重',
-        label: '體重',
-        visible: false,
-      },
-      {
-        field: '體力',
-        label: '體力',
-        visible: false,
-      },
-      {
-        field: '金錢',
-        label: '金錢',
-        visible: false,
-      },
-      {
-        field: '命中',
-        label: '命中',
-        visible: false,
-      },
-      {
-        field: '傷害',
-        label: '傷害',
-        visible: false,
-      },
-      {
-        field: '防禦',
-        label: '防禦',
-        visible: false,
-      },
-      {
-        field: '迴避',
-        label: '迴避',
-        visible: false,
-      },
-      {
-        field: '法傷',
-        label: '法傷',
-        visible: false,
-      },
-      {
-        field: '治療',
-        label: '治療',
-        visible: false,
-      },
-      {
-        field: '料理',
-        label: '料理',
-        visible: false,
-      },
-      {
-        field: '採集',
-        label: '採集',
-        visible: false,
-      },
-      {
-        field: '體能',
-        label: '體能',
-        visible: false,
-      },
-      {
-        field: '偵查',
-        label: '偵查',
-        visible: false,
-      },
-      {
-        field: '鑑定',
-        label: '鑑定',
-        visible: false,
-      },
-      {
-        field: '鍛造',
-        label: '鍛造',
-        visible: false,
-      },
-      {
-        field: '開鎖',
-        label: '開鎖',
-        visible: false,
-      },
-      {
-        field: '扒竊',
-        label: '扒竊',
-        visible: false,
-      },
+      ...tabs.map(getField),
     ],
     currentPage: 1,
   }),
