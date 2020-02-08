d49645044d9c83e8e880403ed1965d8495e1a8bb
SheiUn <me.sheiun@gmail.com>
2020-02-09 05:14:00 +0800

    feat: add auto add version code

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
+const fs = require('fs');
+const packageJson = fs.readFileSync('./package.json');
+const version = JSON.parse(packageJson).version || 0;
 module.exports = {
+  configureWebpack: {
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env': {
+          PACKAGE_VERSION: '"' + version + '"',
+        },
+      }),
+    ],
+  },
   publicPath:
     process.env.NODE_ENV === 'production' ? '/DungeonsDelicacy/' : '/',
   runtimeCompiler: true,
