ac9b3b745e4ff4610cb3632c141d1e3134824c84
SheiUn <me.sheiun@gmail.com>
2020-02-09 12:18:26 +0800

    fix: add webpack

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const webpack = require('webpack');
 const packageJson = fs.readFileSync('./package.json');
 const version = JSON.parse(packageJson).version || 0;
 module.exports = {
