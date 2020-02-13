fefcc1dc36190ef83759569ebd10053f40231d76
SheiUn <me.sheiun@gmail.com>
2020-02-13 16:45:27 +0800

    refactor: cook

--- src/components/Cook.vue
+++ src/components/Cook.vue
@@ -8,46 +8,49 @@
 </template>
 
 <script>
+import { cook } from '../lib/cook';
+
 const ModalForm = {
   props: ['foods'],
   data: () => ({
     foodIndex: 0,
     level: 1,
   }),
-  template: `<form v-on:submit.prevent>
-              <div class="modal-card" style="width: auto">
-                <header class="modal-card-head">
-                  <p class="modal-card-title">料理占卜</p>
-                </header>
-                <section class="modal-card-body">
-                  <b-field label="料理技能等級">
-                    <b-numberinput icon="pot-mix" min="1" v-model="level"></b-numberinput>
-                  </b-field>
+  template: `
+<form v-on:submit.prevent>
+  <div class="modal-card" style="width: auto">
+    <header class="modal-card-head">
+      <p class="modal-card-title">料理占卜</p>
+    </header>
+    <section class="modal-card-body">
+      <b-field label="料理技能等級">
+        <b-numberinput icon="pot-mix" min="1" v-model="level"></b-numberinput>
+      </b-field>
 
-                  <b-field label="食材">
-                    <b-select
-                      placeholder="選擇一個食材"
-                      icon="food-apple"
-                      v-model="foodIndex"
-                      expanded
-                      required
-                    >
-                      <option v-for="(food, index) in foods" :value="index" :key="index">
-                        {{ food.name }}｜{{ food.hard }}｜ {{ food.exp }}
-                      </option>
-                    </b-select>
-                  </b-field>
-                </section>
-                <footer class="modal-card-foot">
-                  <button
-                    @click="$emit('show-cook', {level, food: foods[foodIndex]})"
-                    class="button is-primary"
-                  >
-                    占卜
-                  </button>
-                </footer>
-              </div>
-            </form>`,
+      <b-field label="食材">
+        <b-select
+          placeholder="選擇一個食材"
+          icon="food-apple"
+          v-model="foodIndex"
+          expanded
+          required
+        >
+          <option v-for="(food, index) in foods" :value="index" :key="index">
+            {{ food.name }}｜{{ food.hard }}｜ {{ food.exp }}
+          </option>
+        </b-select>
+      </b-field>
+    </section>
+    <footer class="modal-card-foot">
+      <button
+        @click="$emit('show-cook', {level, food: foods[foodIndex]})"
+        class="button is-primary"
+      >
+        占卜
+      </button>
+    </footer>
+  </div>
+</form>`,
 };
 
 export default {
@@ -97,88 +100,12 @@ export default {
         });
     },
 
-    /* Cook */
-
     showCook(level, food) {
       this.$buefy.dialog.alert({
         title: '占卜結果',
-        message: this.cook(level, food.hard, food.exp),
+        message: cook(level, food.hard, food.exp),
         confirmText: '讚',
       });
-    },
-
-    cook(level = 1, hard = 1, exp = '50/80') {
-      let raw = Number(exp.split('/')[0]);
-      let rate = this.prob(this.cook2dice(level), hard);
-      let successExp = this.getCookBonus(level) * Number(exp.split('/')[1]);
-      let cooked = rate * successExp;
-      let messages = [];
-      messages.push('料理等級: ' + level);
-      messages.push('料理難度: ' + hard);
-      messages.push('生吃/料理: ' + exp);
-      messages.push('料理期望值: ' + cooked);
-      messages.push('料理經驗值: ' + successExp);
-      messages.push('料理成功率: ' + parseInt(rate * 100) + '%');
-      if (raw > cooked) messages.push('建議: 生吃');
-      else if (raw < cooked) messages.push('建議: 熟食');
-      else messages.push('建議: 都行');
-      return messages.join('<br />');
-    },
-
-    /**
-     * @param {Number} num
-     * @param {Number} offset
-     */
-    getDice(num, offset = 0) {
-      return Array.from(Array(num).keys()).map((n) => n + offset + 1);
-    },
-
-    prob(a, b) {
-      if (typeof a == 'number' && typeof b == 'number') {
-        return Number(a >= b);
-      } else if (typeof a == 'number') {
-        return b.filter((i) => a >= i).length / b.length;
-      } else if (typeof b == 'number') {
-        return a.filter((i) => i >= b).length / a.length;
-      } else {
-        let count = 0;
-        a.forEach((i) => {
-          b.forEach((j) => {
-            if (i >= j) count++;
-          });
-        });
-        return count / (a.length * b.length);
-      }
-    },
-
-    cook2dice(level) {
-      let dice;
-      if (level < 1) {
-        throw new Error('你不能料理', level);
-      } else if (level <= 2) {
-        dice = 4;
-      } else {
-        dice = 6;
-      }
-      return this.getDice(Number(dice), Number(level));
-    },
-
-    getCookBonus(level) {
-      let bonus;
-      if (level < 1) {
-        throw new Error('你不能料理', level);
-      } else if (4 >= level) {
-        bonus = 1.0;
-      } else if (7 >= level) {
-        bonus = 1.2;
-      } else if (8 >= level) {
-        bonus = 1.5;
-      } else if (11 >= level) {
-        bonus = 1.7;
-      } else if (15 >= level) {
-        bonus = 2.0;
-      }
-      return bonus;
     },
   },
 };
---
+++ src/lib/cook.js
@@ -0,0 +1,73 @@
+export const cook = (level = 1, hard = 1, exp = '50/80') => {
+  let raw = Number(exp.split('/')[0]);
+  let rate = prob(cook2dice(level), hard);
+  let successExp = getCookBonus(level) * Number(exp.split('/')[1]);
+  let cooked = rate * successExp;
+  let messages = [];
+  messages.push('料理等級: ' + level);
+  messages.push('料理難度: ' + hard);
+  messages.push('生吃/料理: ' + exp);
+  messages.push('料理期望值: ' + cooked);
+  messages.push('料理經驗值: ' + successExp);
+  messages.push('料理成功率: ' + parseInt(rate * 100) + '%');
+  if (raw > cooked) messages.push('建議: 生吃');
+  else if (raw < cooked) messages.push('建議: 熟食');
+  else messages.push('建議: 都行');
+  return messages.join('<br />');
+};
+
+/**
+ * @param {Number} num
+ * @param {Number} offset
+ */
+const getDice = (num, offset = 0) => {
+  return Array.from(Array(num).keys()).map((n) => n + offset + 1);
+};
+
+const prob = (a, b) => {
+  if (typeof a == 'number' && typeof b == 'number') {
+    return Number(a >= b);
+  } else if (typeof a == 'number') {
+    return b.filter((i) => a >= i).length / b.length;
+  } else if (typeof b == 'number') {
+    return a.filter((i) => i >= b).length / a.length;
+  } else {
+    let count = 0;
+    a.forEach((i) => {
+      b.forEach((j) => {
+        if (i >= j) count++;
+      });
+    });
+    return count / (a.length * b.length);
+  }
+};
+
+const cook2dice = (level) => {
+  let dice;
+  if (level < 1) {
+    throw new Error('你不能料理', level);
+  } else if (level <= 2) {
+    dice = 4;
+  } else {
+    dice = 6;
+  }
+  return getDice(Number(dice), Number(level));
+};
+
+const getCookBonus = (level) => {
+  let bonus;
+  if (level < 1) {
+    throw new Error('你不能料理', level);
+  } else if (4 >= level) {
+    bonus = 1.0;
+  } else if (7 >= level) {
+    bonus = 1.2;
+  } else if (8 >= level) {
+    bonus = 1.5;
+  } else if (11 >= level) {
+    bonus = 1.7;
+  } else if (15 >= level) {
+    bonus = 2.0;
+  }
+  return bonus;
+};
