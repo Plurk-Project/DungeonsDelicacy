5e7568c32bdee975a38c02cc97e38eb69ddb6bcd
SheiUn <me.sheiun@gmail.com>
2020-02-17 00:38:09 +0800

    Updates

---
+++ precache-manifest.32d8b67acd90ccb8882e20b57694f776.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b25f32905c1b7a56df8c",
+    "url": "/DungeonsDelicacy/css/app.1edca1ef.css"
+  },
+  {
+    "revision": "5a021ab95b4d2a0c8e7511c40ca7bb2f",
+    "url": "/DungeonsDelicacy/img/avatar.5a021ab9.png"
+  },
+  {
+    "revision": "977f1a5e8503ef2854bac41b202a51ac",
+    "url": "/DungeonsDelicacy/img/easy-card.977f1a5e.png"
+  },
+  {
+    "revision": "e245273bd69e3e102382c5b37f6164a6",
+    "url": "/DungeonsDelicacy/img/logo.e245273b.png"
+  },
+  {
+    "revision": "e7bd0ccea44d49181e05aa318bfc8b05",
+    "url": "/DungeonsDelicacy/index.html"
+  },
+  {
+    "revision": "b25f32905c1b7a56df8c",
+    "url": "/DungeonsDelicacy/js/app.0fe1ba49.js"
+  },
+  {
+    "revision": "780188f87d3e21d541be",
+    "url": "/DungeonsDelicacy/js/chunk-vendors.861b2f60.js"
+  },
+  {
+    "revision": "d73523cf4c78a95f7596ad704f9de71d",
+    "url": "/DungeonsDelicacy/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/DungeonsDelicacy/robots.txt"
+  }
+]);
