61834cd904b10a48774a5261a3826670388c46e7
SheiUn <me.sheiun@gmail.com>
2020-03-07 16:14:12 +0800

    Updates

--- precache-manifest.9237ac22edc118015b05d3f1ee355501.js
+++ precache-manifest.9237ac22edc118015b05d3f1ee355501.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "78c278a4564c4bf8ee57",
+    "revision": "88e639d28ccec6fb1845",
     "url": "/css/app.c9a3e1c8.css"
   },
   {
@@ -16,12 +16,12 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/img/logo.e245273b.png"
   },
   {
-    "revision": "a73814e5ba933366a180f2a0f44c9823",
+    "revision": "bf99afeebb47bc566f88950654cc82a1",
     "url": "/index.html"
   },
   {
-    "revision": "78c278a4564c4bf8ee57",
-    "url": "/js/app.e213ec64.js"
+    "revision": "88e639d28ccec6fb1845",
+    "url": "/js/app.4e714f7b.js"
   },
   {
     "revision": "6136d928e8065e6b79b2",
--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
-importScripts("/precache-manifest.f2ba4372adbd4801547c7621e4635808.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/precache-manifest.9237ac22edc118015b05d3f1ee355501.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 workbox.core.setCacheNameDetails({ prefix: 'd4' });
 
-const LATEST_VERSION = '2.3.5';
+const LATEST_VERSION = '2.3.6';
 
 self.addEventListener('activate', (event) => {
   console.log(`%c ${LATEST_VERSION} `, 'background: #ddd; color: #0000ff');
