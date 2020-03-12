9fb7b1ee0e02b1d70fed370b440c122bc4e5d0f0
SheiUn <me.sheiun@gmail.com>
2020-03-13 02:12:51 +0800

    feat: add my ads

--- src/components/Rank.vue
+++ src/components/Rank.vue
@@ -3,6 +3,16 @@
     <b-tabs v-model="activeTab" position="is-centered" class="is-marginless">
       <b-tab-item v-for="tab in tabs" :key="tab" :label="tab"> </b-tab-item>
     </b-tabs>
+    <div class="columns is-centered" v-if="getActiveTabName() == '鑑定'">
+      <b-tooltip label="生意冷清 趁機偷打廣告 非急件有半價優惠（請備註）">
+        <a
+          href="https://www.dungeonsdelicacy.com/forum/mei-shi-zhuo-jin/jian-ding-pu-9-deng"
+          target="_blank"
+        >
+          【廣告】美食桌巾 - 艾斯·克林姆 9 級鑑定舖
+        </a>
+      </b-tooltip>
+    </div>
     <div class="is-padding">
       <div class="columns is-centered">
         <div class="column is-half">
