fcccb42b1246227d890aa9aabef76c3dc4bfc68c
SheiUn <me.sheiun@gmail.com>
2020-03-13 21:22:41 +0800

    fix: cennter ads

--- src/components/Rank.vue
+++ src/components/Rank.vue
@@ -3,7 +3,7 @@
     <b-tabs v-model="activeTab" position="is-centered" class="is-marginless">
       <b-tab-item v-for="tab in tabs" :key="tab" :label="tab"> </b-tab-item>
     </b-tabs>
-    <div class="columns is-centered" v-if="getActiveTabName() == '鑑定'">
+    <div class="has-text-centered" v-if="getActiveTabName() == '鑑定'">
       <b-tooltip label="生意冷清 趁機偷打廣告 非急件有半價優惠（請備註）">
         <a
           href="https://www.dungeonsdelicacy.com/forum/mei-shi-zhuo-jin/jian-ding-pu-9-deng"
